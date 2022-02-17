0fedd4ab60eb6563db9245624b6b809504410b84
zenglinsheng <996433583@qq.com>
2022-02-17 17:42:46 +0800

    modify on 2022.2.17

--- src/main/java/test/CASTest.java
+++ src/main/java/test/CASTest.java
@@ -1,7 +1,11 @@
 package test;
 
 import org.junit.Test;
+import sun.misc.Unsafe;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -11,7 +15,15 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class CASTest {
 
-    AtomicInteger i = new AtomicInteger(0);
+    Food food = new Food("apple", new AtomicInteger(10_000_000));
+
+    AtomicInteger lock = new AtomicInteger(0);
+
+    int i = 0;
+
+    AtomicBoolean flag = new AtomicBoolean(false);
+
+    int count = 50;
 
     @Test
     public void test() throws InterruptedException {
@@ -23,16 +35,177 @@ public class CASTest {
         thread01.join();
         thread02.join();
 
+        System.out.println("i = " + food.count.get());
+    }
+
+    @Test
+    public void test02() throws InterruptedException {
+        var startTime = System.currentTimeMillis();
+        var thread01 = new Thread(new B(count));
+        var thread02 = new Thread(new B(count));
+
+        thread01.start();
+        thread02.start();
+
+        thread01.join();
+        thread02.join();
+
+        System.out.println("i = " + i);
+        System.out.println("time = " + (System.currentTimeMillis() - startTime));
+    }
+
+    @Test
+    public void test_02_01() throws InterruptedException {
+        var startTime = System.currentTimeMillis();
+        var thread01 = new Thread(new D(count));
+        var thread02 = new Thread(new D(count));
+
+        thread01.start();
+        thread02.start();
+
+        thread01.join();
+        thread02.join();
+
+        System.out.println("i = " + i);
+        System.out.println("time = " + (System.currentTimeMillis() - startTime));
+    }
+
+   @Test
+    public void test03() throws InterruptedException {
+        var thread01 = new Thread(new C());
+        var thread02 = new Thread(new C());
+
+        thread01.start();
+        thread02.start();
+
+        thread01.join();
+        thread02.join();
+
         System.out.println("i = " + i);
     }
 
-    class A implements Runnable {
+    @Test
+    public void test04() throws InterruptedException {
+        var startTime = System.currentTimeMillis();
 
+        var count = 20;
+        final var latch = new CountDownLatch(count);
+        var service = Executors.newFixedThreadPool(count);
+
+        for (int i = 0;i < count;i ++) {
+            service.submit(new E(latch, false));
+        }
+
+        latch.await();
+        System.out.println("i = " + i);
+        System.out.println("耗时：" + (System.currentTimeMillis() - startTime));
+    }
+
+    private void enter() {
+        while (!lock.compareAndSet(0, 1)) {}
+    }
+
+    private void leave() {
+        lock.compareAndSet(1, 0);
+    }
+
+    private void enterByFlag() {
+        while (!flag.compareAndSet(false, true)) {}
+    }
+
+    private void leaveByFlag() {
+        flag.compareAndSet(true, false);
+    }
+
+    class Food {
+        private String name;
+        private AtomicInteger count;
+
+        public Food(String name, AtomicInteger count) {
+            this.name = name;
+            this.count = count;
+        }
+    }
+
+    class A implements Runnable {
         @Override
         public void run() {
             for (int j = 0;j < 1000000;j ++) {
-                i.incrementAndGet();
+                food.count.decrementAndGet();
             }
+        }
+    }
+
+    class B implements Runnable {
+        private int count;
+
+        public B(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            for (int j = 0;j < count;j ++) {
+                enter();
+                i ++;
+                leave();
+            }
+        }
+    }
+
+    class C implements Runnable {
+        @Override
+        public void run() {
+            for (int j = 0;j < 1_000_000;j ++) {
+                enterByFlag();
+                i ++;
+                leaveByFlag();
+            }
+        }
+    }
+
+    class D implements Runnable {
+        private int count;
+
+        public D(int count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            for (int j = 0;j < count;j ++) {
+                synchronized (D.class) {
+                    i ++;
+                }
+            }
+        }
+    }
+
+    class E implements Runnable {
+        private CountDownLatch latch;
+        private boolean isSync;
+
+        public E(CountDownLatch latch, boolean isSync) {
+            this.latch = latch;
+            this.isSync = isSync;
+        }
+
+        @Override
+        public void run() {
+            if (isSync) {
+                for (int j = 0;j < count;j ++) {
+                    synchronized (E.class) {
+                        i ++;
+                    }
+                }
+            } else {
+                for (int j = 0;j < count;j ++) {
+                    enter();
+                    i ++;
+                    leave();
+                }
+            }
+            latch.countDown();
         }
     }
 
--- src/main/java/test/SynchronizedKeyWords.java
+++ src/main/java/test/SynchronizedKeyWords.java
@@ -1,6 +1,7 @@
 package test;
 
-import java.util.concurrent.ArrayBlockingQueue;
+import org.junit.Test;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -12,21 +13,46 @@ public class SynchronizedKeyWords {
         var t = new Thread(() -> {
             try {
                 lock.tryLock(1000, TimeUnit.MICROSECONDS);
+                Thread.sleep(2000);
+                System.out.println("lock...");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             synchronized (lock) {
                 try {
-                    Thread.sleep(10000);
+                    Thread.sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("finished.");
+                System.out.println("finished...");
             }
         });
         t.start();
         t.interrupt();
-
-
     }
+
+    @Test
+    public void test() throws InterruptedException {
+        var lock = new ReentrantLock();
+
+        var t = new Thread(() -> {
+            int k = 0;
+            try {
+                lock.lockInterruptibly();
+                for(int i = 0; i < 1000000000; i ++) {
+                    k += 1;
+                }
+                System.out.println("finished:" + k);
+            } catch (InterruptedException e) {
+                System.out.println("intterrupt:" + k);
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        });
+        t.start();
+        t.interrupt();
+        t.join();
+    }
+
 }
